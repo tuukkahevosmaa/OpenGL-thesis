b2e5a2034909cf707530dd219de26804d5cf486e
tuukkahevosmaa <thevosmaa@gmail.com>
2017-02-09 20:54:50 +0200

    Boilerplate done, move to rendering

--- OpenGLThesis/main.cpp
+++ OpenGLThesis/main.cpp
@@ -1,13 +1,41 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <SDL.h>
+#include <SDL_opengl.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-	//init test
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
-	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(window);
 
-	SDL_Delay(1000);
+	glewExperimental = GL_TRUE;
+	glewInit();
 
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+
+	printf("%u\n", vertexBuffer);
+
+	SDL_Event windowEvent;
+	while (true)
+	{
+		if (SDL_PollEvent(&windowEvent))
+		{
+			if (windowEvent.type == SDL_QUIT) break;
+		}
+
+		SDL_GL_SwapWindow(window);
+	}
+
+	SDL_GL_DeleteContext(context);
 	SDL_Quit();
+
 	return 0;
 }
