6245e0ced3b1671afa4a1ba0a49736745416c553
tuukkahevosmaa <thevosmaa@gmail.com>
2017-03-24 10:43:09 +0200

    Color

--- OpenGLThesis/main.cpp
+++ OpenGLThesis/main.cpp
@@ -8,23 +8,26 @@
 const GLchar* vertexSource =
 "#version 440\n"
 "in vec2 position;"
+"in vec3 color;"
+"out vec3 Color;"
 "void main()"
 "{"
+"Color = color;"
 "gl_Position = vec4(position, 0.0, 1.0);"
 "}";
 
 const GLchar* fragmentSource =
 "#version 440\n"
-"uniform vec3 triangleColor;"
+"in vec3 Color;"
 "out vec4 outColor;"
 "void main()"
 "{"
-"outColor = vec4(triangleColor, 1.0);"
+"outColor = vec4(Color, 1.0);"
 "}";
 
 int main(int argc, char *argv[])
 {
-	auto t_start = std::chrono::high_resolution_clock::now();
+	//auto t_start = std::chrono::high_resolution_clock::now();
 
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
@@ -43,9 +46,9 @@ int main(int argc, char *argv[])
 	glBindVertexArray(vao);
 
 	float vertices[] = {
-		0.0f, 0.5f,
-		0.5f, -0.5f,
-		-0.5f, -0.5f
+		0.0f, 0.5f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f
 	};
 
 	GLuint vbo;
@@ -73,11 +76,12 @@ int main(int argc, char *argv[])
 	glUseProgram(shaderProgram);
 
 	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(posAttrib);
-
-	GLint uniColor = glGetUniformLocation(shaderProgram, "triangleColor");
-	glUniform3f(uniColor, 1.0f, 0.0f, 0.0f);
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), 0);
+	
+	GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+	glEnableVertexAttribArray(colAttrib);
+	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2*sizeof(float)));
 
 	SDL_Event windowEvent;
 	while (true)
@@ -87,10 +91,10 @@ int main(int argc, char *argv[])
 			if (windowEvent.type == SDL_QUIT) break;
 		}
 
-		auto t_now = std::chrono::high_resolution_clock::now();
-		float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
+		//auto t_now = std::chrono::high_resolution_clock::now();
+		//float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
 
-		glUniform3f(uniColor, (sin(time*4.0f) + 1.0f) / 2.0f, 0.0f, 0.0f);
+		//glUniform3f(uniColor, (sin(time*4.0f) + 1.0f) / 2.0f, 0.0f, 0.0f);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
