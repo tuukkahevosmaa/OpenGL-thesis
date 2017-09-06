8b4de94579ddd6ca1ba91021b44c94a9f978a508
tuukkahevosmaa <thevosmaa@gmail.com>
2017-09-06 18:25:01 +0300

    Final lighting etc.

--- OpenGLThesis/main.cpp
+++ OpenGLThesis/main.cpp
@@ -8,8 +8,6 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <stdio.h>
 #include <iostream>
-#include <vector>
-#include <chrono>
 
 const GLchar* vertexSource =
 "#version 440\n"
@@ -28,9 +26,9 @@ const GLchar* vertexSource =
 "{"
 "Color = color;"
 "Texcoord = texcoord;"
-"Normal = normal;"
 "FragPos = vec3(model * vec4(position, 1.0));"
-"gl_Position = proj * view * model * vec4(position, 1.0);"
+"Normal = mat3(transpose(inverse(model))) * normal;"
+"gl_Position = proj * view * model * vec4(FragPos, 1.0);"
 "}";
 
 const GLchar* fragmentSource =
@@ -46,18 +44,19 @@ const GLchar* fragmentSource =
 "uniform vec3 viewPos;"
 "void main()"
 "{"
-"float ambientStrength = 0.1;"
+"float ambientStrength = 0.15;"
 "vec3 ambient = ambientStrength * lightColor;"
 "vec3 norm = normalize(Normal);"
 "vec3 lightDir = normalize(lightPos - FragPos);"
 "float diff = max(dot(norm, lightDir), 0.0);"
 "vec3 diffuse = diff * lightColor;"
-"float specularStrength = 0.5;"
-"vec3 viewDir = normalize(viewPos - FragPos);"
+"float specularStrength = 1.5;"
+"vec3 viewDir = normalize(normalize(viewPos) - FragPos);"
 "vec3 reflectDir = reflect(-lightDir, norm);"
-"float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);"
+"float spec = pow(max(dot(viewDir, reflectDir), 0.0), 8);"
 "vec3 specular = specularStrength * spec * lightColor;"
-"vec3 result = (ambient + diffuse + specular) * Color;"
+"vec3 result = (ambient + diffuse + specular) * texture(tex, Texcoord*6).rgb;"
+//"vec3 result = (ambient + diffuse + specular) * Color;"
 //"outColor = texture(tex, Texcoord);"
 "outColor = vec4(result, 1.0);"
 "}";
@@ -100,47 +99,47 @@ int main(int argc, char *argv[])
 
 	GLfloat vertices[] = {
 	//  Position			Color             Texcoord.    Normal
-		-0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f,
-		 0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, -1.0f,
-		 0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, -1.0f,
-		 0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, -1.0f,
-		-0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, -1.0f,
-		-0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f,
+		-0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f,
+		 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 1.0f, 0.0f, 0.0f, 0.0f, -1.0f,
+		 0.5f,  0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 1.0f, -1.0f, 0.0f, 0.0f, -1.0f,
+		 0.5f,  0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 1.0f, -1.0f, 0.0f, 0.0f, -1.0f,
+		-0.5f,  0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f,
+		-0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f,
 
-		-0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-		 0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-		 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-		 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
 
-		-0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, -1.0f, 0.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, -1.0f, 0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 1.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 0.0f, -1.0f, -1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 1.0f, 0.0f, -1.0f, 0.0f, 0.0f,
 
-		 0.5f,  0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-		 0.5f,  0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-		 0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-		 0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 1.0f, -1.0f, 1.0f, 0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
 
-		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, -1.0f, 0.0f,
-		 0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, -1.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, -1.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, -1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, -1.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 1.0f, -1.0f, 0.0f, -1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 1.0f, 0.0f, 0.0f, -1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 1.0f, 0.0f, 0.0f, -1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 0.0f, -1.0f, 0.0f, -1.0f, 0.0f,
 
-		-0.5f,  0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
-		 0.5f,  0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f
+		-0.5f,  0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f, 0.5f, 0.5f, 0.1f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f, 0.5f, 0.5f, 0.1f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f
 	};
 
 	GLfloat lightVertices[] = {
@@ -259,15 +258,15 @@ int main(int argc, char *argv[])
 
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, textures[0]);
-	image = SOIL_load_image("sample.png", &width, &height, 0, SOIL_LOAD_RGB);
+	image = SOIL_load_image("dottest.png", &width, &height, 0, SOIL_LOAD_RGB);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
 	SOIL_free_image_data(image);
 	//glUniform1i(glGetUniformLocation(shaderProgram, "tex"), 0);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	/*glActiveTexture(GL_TEXTURE1);
 	glBindTexture(GL_TEXTURE_2D, textures[1]);
@@ -290,7 +289,9 @@ int main(int argc, char *argv[])
 	GLint uniModel = glGetUniformLocation(shaderProgram, "model");
 	glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));*/
 
-	glm::vec3 viewPos(1.0f, 1.0f, 1.5f);
+	glm::vec3 viewPos(-1.0f, 0.8f, 1.5f);
+	GLint uniViewPos = glGetUniformLocation(shaderProgram, "viewPos");
+	glUniform3fv(uniViewPos, 1, glm::value_ptr(viewPos));
 
 	glm::mat4 view = glm::lookAt(
 		viewPos,
@@ -299,9 +300,6 @@ int main(int argc, char *argv[])
 	);
 	GLint uniView = glGetUniformLocation(shaderProgram, "view");
 	glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(view));
-
-	GLint uniViewPos = glGetUniformLocation(shaderProgram, "viewPos");
-	glUniform3fv(uniViewPos, 1, glm::value_ptr(viewPos));
 
 	glm::mat4 proj = glm::perspective(glm::radians(80.0f), 1024.0f / 768.0f, 0.5f, 10.0f);
 	GLint uniProj = glGetUniformLocation(shaderProgram, "proj");
@@ -313,7 +311,7 @@ int main(int argc, char *argv[])
 	GLint uniLightColor = glGetUniformLocation(shaderProgram, "lightColor");
 	glUniform3fv(uniLightColor, 1, glm::value_ptr(lightColor));
 
-	glm::vec3 lightPos(1.0f, 1.0f, 1.5f);
+	glm::vec3 lightPos(-0.2f, 0.8f, 1.5f);
 	GLint uniLightPos = glGetUniformLocation(shaderProgram, "lightPos");
 	glUniform3fv(uniLightPos, 1, glm::value_ptr(lightPos));
 
@@ -336,19 +334,11 @@ int main(int argc, char *argv[])
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glm::mat4 model;
-		model = glm::rotate(model, glm::radians(0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		//model = glm::rotate(model, glm::radians(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		GLint uniModel = glGetUniformLocation(shaderProgram, "model");
 		glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		/*model = glm::translate(model, lightPos);
-		model = glm::scale(model, glm::vec3(0.15f));
-		uniModel = glGetUniformLocation(shaderProgram, "model");
-		glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
-
-		glDrawArrays(GL_TRIANGLES, 0, 36);*/
-
 
 		SDL_GL_SwapWindow(window);
 	}
